4f4a787351c9b52c4720bc61c9a04a4af32acf15
Adam Satan <satanadam@outlook.com>
2020-07-18 18:54:22 +0200

    Initial commit

---
+++ src/main/java/hu/satan/photolibrary/PhotolibraryApplication.java
@@ -0,0 +1,13 @@
+package hu.satan.photolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotolibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotolibraryApplication.class, args);
+    }
+
+}
